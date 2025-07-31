77f16dbad8501a39660c2505058727b0c18bbd1b
rafeekshaik <rafeekshaik365@gmail.com>
2025-07-31 16:07:59 +0530

    vpc

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr" {
+ 
+}
+variable "enable_dns_hostnames" {
+  
+}
+
+variable "project_name" {
+  
+}
+variable "environment" {
+  
+}
+variable "common_tags" {
+  
+}
+variable "vpc_tags" {
+default = {}
+}
